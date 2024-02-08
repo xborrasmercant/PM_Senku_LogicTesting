3ef516adb6f3ae7c48358c5e28cdcd125a186e45
Xavier <chabolaxavi@gmail.com>
2024-02-08 11:31:21 +0100

    Basic logic finished for playing

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,47 +1,148 @@
+import java.util.Scanner;
+
 public class Board {
-    private int[][] boardMatrix;
+    private Cell[][] boardMatrix;
+    public enum Direction {
+        UP, DOWN, RIGHT, LEFT;
+    }
+
 
     public Board () {
-        boardMatrix = new int[7][7];
+        boardMatrix = new Cell[7][7];
         initBoard();
     }
 
 
+    public void moveCell(Cell selectedCell, Cell targetCell, Direction movementDirection) {
+
+        switch (movementDirection) {
+            case DOWN -> handleMovement(selectedCell, targetCell, selectedCell.getRowPos()+1, selectedCell.getColPos());
+            case UP -> handleMovement(selectedCell, targetCell, selectedCell.getRowPos()-1, selectedCell.getColPos()+1);
+            case RIGHT -> handleMovement(selectedCell, targetCell, selectedCell.getRowPos(), selectedCell.getColPos()+1);
+            case LEFT -> handleMovement(selectedCell, targetCell, selectedCell.getRowPos(), selectedCell.getColPos()-1);
+        }
+    }
+
+    public void handleMovement(Cell selectedCell, Cell targetCell, int row, int col) {
+        selectedCell.setValue(0);
+        boardMatrix[row][col].setValue(0); // Empty cell between selected and target cells
+        targetCell.setValue(1);
+    }
+
+    public Direction getDirection(Cell selectedCell, Cell targetCell) {
+
+        // Check the difference result of selected and target pegs.
+        int rowDif = selectedCell.getRowPos() - targetCell.getRowPos(), colDif = selectedCell.getColPos() - targetCell.getColPos();
+
+
+        if (rowDif > 2 || rowDif < -2 || colDif > 2 || colDif < -2) {
+            return null;
+        }
+
+        if (colDif == 0) {
+            return switch (rowDif) {
+                case -2 -> Direction.DOWN;
+                case 2 -> Direction.UP;
+                default -> null;
+            };
+        }
+
+        return switch (colDif) {
+            case -2 -> Direction.RIGHT;
+            case 2 -> Direction.LEFT;
+            default -> null;
+        };
+    }
+
     public void initBoard() {
 
         // Fill with pegs
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                boardMatrix[i][j] = 1;
+        for (int row = 0; row < 7; row++) {
+            for (int col = 0; col < 7; col++) {
+                boardMatrix[row][col] = new Cell(col, row, 1);
             }
         }
 
         // Define borders of board
         int[][] borders = new int[][]{ {0, 0}, {0, 1}, {1, 0}, {1, 1}, {0, 5}, {0, 6}, {1, 5}, {1, 6}, {5, 0}, {5, 1}, {6, 0}, {6, 1}, {5, 5}, {5, 6}, {6, 5}, {6, 6}};
         for (int[] pos : borders) {
-            boardMatrix[pos[0]][pos[1]] = -1;
+            boardMatrix[pos[0]][pos[1]].setValue(-1);
         }
 
         // Set the empty position of the board (center)
-        boardMatrix[3][3] = 0;
+        boardMatrix[3][3].setValue(0);
     }
+    public void cleanTerminal(){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
+    }
     public void printBoard() {
-        for (int[] row : boardMatrix) {
-            for (int cell : row) {
-                if (cell == -1) {
+        cleanTerminal();
+
+        for (Cell[] row : boardMatrix) {
+            for (Cell peg : row) {
+                if (peg.getValue() == -1) {
                     System.out.print("  ");
                 } else {
-                    System.out.print(cell + " ");
+                    System.out.print(peg.getValue() + " ");
                 }
             }
             System.out.println();
         }
+        System.out.println();
+
     }
 
     public static void main(String[] args) {
         Board gameBoard = new Board();
+        Scanner in = new Scanner(System.in);
 
         gameBoard.printBoard();
+        while (true) {
+            try {
+                System.out.println("Select one peg to move (row col): ");
+                int selectedRow = in.nextInt();
+                int selectedCol = in.nextInt();
+                Cell selectedCell = gameBoard.boardMatrix[selectedRow][selectedCol];
+
+                if (selectedCell.getValue() != 1) {
+                    System.out.println("Invalid selection, please select a peg.");
+                    continue;
+                }
+
+                System.out.println("Select target position (row col): ");
+                int targetRow = in.nextInt();
+                int targetCol = in.nextInt();
+                Cell targetCell = gameBoard.boardMatrix[targetRow][targetCol];
+
+                if (targetCell.getValue() != 0) {
+                    System.out.println("Invalid target, please select an empty cell.");
+                    continue;
+                }
+
+                Direction movementDirection = gameBoard.getDirection(selectedCell, targetCell);
+                if (movementDirection != null) {
+                    gameBoard.moveCell(selectedCell, targetCell, movementDirection);
+                    gameBoard.printBoard();
+                } else {
+                    System.out.println("Invalid move, please try again.");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input, please enter valid row and column numbers.");
+                in.nextLine(); // Clear the buffer
+            }
+        }
     }
 }
---
+++ src/main/java/Cell.java
@@ -0,0 +1,28 @@
+public class Cell {
+    private int colPos, rowPos, value;
+
+    public Cell (int colPos, int rowPos, int value) {
+        this.colPos = colPos;
+        this.rowPos = rowPos;
+        this.value = value;
+    }
+
+    public int getColPos() {
+        return colPos;
+    }
+    public void setColPos(int colPos) {
+        this.colPos = colPos;
+    }
+    public int getRowPos() {
+        return rowPos;
+    }
+    public void setRowPos(int rowPos) {
+        this.rowPos = rowPos;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
